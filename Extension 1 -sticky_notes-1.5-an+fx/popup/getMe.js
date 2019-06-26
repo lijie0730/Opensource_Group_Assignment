3bc6a7d2be1fd9e52731afa98a787aa033f73f80
lijie0730 <34046940+lijie0730@users.noreply.github.com>
2019-06-26 09:09:34 +0800

    Update getMe.js

--- Extension 1 -sticky_notes-1.5-an+fx/popup/getMe.js
+++ Extension 1 -sticky_notes-1.5-an+fx/popup/getMe.js
@@ -94,3 +94,4 @@ document.getElementById('button_time').addEventListener("click", function(){
     if(timer_checked == 1){
         document.getElementById("timer_function").removeAttribute("hidden");
       timer_checked = 0;
+        }else{ 
