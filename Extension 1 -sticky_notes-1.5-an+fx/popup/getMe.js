5e96688ca4ed6618805f9ebe8003d400f30ec652
lijie0730 <34046940+lijie0730@users.noreply.github.com>
2019-06-26 21:31:52 +0800

    Update getMe.js

--- Extension 1 -sticky_notes-1.5-an+fx/popup/getMe.js
+++ Extension 1 -sticky_notes-1.5-an+fx/popup/getMe.js
@@ -103,3 +103,4 @@ document.getElementById('button_time').addEventListener("click", function(){
 document.getElementById('start_countdown').addEventListener("click", function(){
   var countdown_value = document.getElementById("input_countdown").value;
     var inputminute = countdown_value;
+   var duration = inputminute*60;
