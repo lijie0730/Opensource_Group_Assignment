71115e25efe20bafc6de59a6e7afdbf5fd44a21e
lijie0730 <34046940+lijie0730@users.noreply.github.com>
2019-06-30 14:48:00 +0800

    Update time.js

--- Extension 1 -sticky_notes-1.5-an+fx/popup/time.js
+++ Extension 1 -sticky_notes-1.5-an+fx/popup/time.js
@@ -37,3 +37,14 @@ function drawNumbers(ctx, radius) {
   ctx.font = radius*0.15 + "px arial";
   ctx.textBaseline="middle";
   ctx.textAlign="center";
+    for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
