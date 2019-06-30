40e8c2227dcb6936eb859255648eecde5d92a462
lijie0730 <34046940+lijie0730@users.noreply.github.com>
2019-06-30 14:49:13 +0800

    Update time.js

--- Extension 1 -sticky_notes-1.5-an+fx/popup/time.js
+++ Extension 1 -sticky_notes-1.5-an+fx/popup/time.js
@@ -32,21 +32,21 @@ function drawFace(ctx, radius) {
 }
 
 function drawNumbers(ctx, radius) {
-  var ang;
-  var num;
-  ctx.font = radius*0.15 + "px arial";
-  ctx.textBaseline="middle";
-  ctx.textAlign="center";
+    var ang;
+    var num;
+    ctx.font = radius*0.15 + "px arial";
+    ctx.textBaseline="middle";
+    ctx.textAlign="center";
     for(num = 1; num < 13; num++){
-    ang = num * Math.PI / 6;
-    ctx.rotate(ang);
-    ctx.translate(0, -radius*0.85);
-    ctx.rotate(-ang);
-    ctx.fillText(num.toString(), 0, 0);
-    ctx.rotate(ang);
-    ctx.translate(0, radius*0.85);
-    ctx.rotate(-ang);
-  }
+      ang = num * Math.PI / 6;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius*0.85);
+      ctx.rotate(-ang);
+      ctx.fillText(num.toString(), 0, 0);
+      ctx.rotate(ang);
+      ctx.translate(0, radius*0.85);
+      ctx.rotate(-ang);
+    }
 }
 
 function drawTime(ctx, radius){
@@ -54,7 +54,7 @@ function drawTime(ctx, radius){
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
-      //hour
+    //hour
     hour=hour%12;
     hour=(hour*Math.PI/6)+
     (minute*Math.PI/(6*60))+
